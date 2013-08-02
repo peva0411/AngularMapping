0412dcc77db1c1f247167b24f4d0a4f1f3f61cf3
Phil <peva0411@gmail.com>
2013-08-02 07:38:31 -0400

    First commit

---
+++ AngularMapping/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularMapping.MvcApplication" Language="C#" %>
---
+++ AngularMapping/Scripts/App/app.js
@@ -0,0 +1,34 @@
+﻿var mapApp = angular.module("mapApp", ['google-maps']);
+
+mapApp.controller("mapCntrl", function mapCntrl($scope, $http) {
+    angular.extend($scope, {
+        center: {
+            latitude: 0,
+            longitude: 0,
+        },
+        markers: [],
+        zoom: 8,
+    });
+
+    $scope.Address = "";
+    $scope.count = 0;
+    $scope.status = "";
+    $scope.geocoder = new google.maps.Geocoder();
+    $scope.mapResults = [];
+
+    $scope.Geocode = function() {
+        $scope.geocoder.geocode({
+                'address': $scope.Address
+            },
+            function(results, status) {
+                console.log(results);
+                console.log(status);
+                if (status == google.maps.GeocoderStatus.OK) {
+                    $scope.count = results.length;
+                    $scope.mapResults = results;
+                } else {
+                    $scope.status = "Geocode was not successful: " + status;
+                }
+            });
+    }
+});
